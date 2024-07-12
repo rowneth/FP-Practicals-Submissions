ce65cc5d36534e5ea95be08248bfd6babb05f083
Don Roneth <113205821+rowneth@users.noreply.github.com>
2024-07-12 10:48:09 +0530

    Create q1-1.scala

---
+++ lab5/q1-1.scala
@@ -0,0 +1,21 @@
+object InventoryManagement {
+
+    def displayInventory(itemNames: Array[String], itemQuantity: Array[Int]): Unit = {
+        if (itemNames.length != itemQuantity.length){
+            println("Error: Item names and quantities array should be same length!")
+            return
+        }
+
+        println("Inventory:")
+        for(i <- itemNames.indices){
+            println(s"${itemNames(i)}: ${itemQuantity(i)}")
+        }
+    }
+
+    def main(args: Array[String]): Unit = {
+        val itemNames = Array("Mangoes", "Banana", "Apples", "Grapes")
+        val itemQuantity = Array(9, 5, 6, 15)
+
+      displayInventory(itemNames, itemQuantity)
+    }
+}
