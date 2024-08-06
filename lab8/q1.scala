3a513ba5a41cb2c82a6fcdd1f7472fc0256f9e4d
Don Roneth <113205821+rowneth@users.noreply.github.com>
2024-08-06 10:29:21 +0530

    Update q1.scala

--- lab8/q1.scala
+++ lab8/q1.scala
@@ -1,71 +1,31 @@
-import scala.io.StdIn._
-import scala.collection.mutable.ListBuffer
-
-object lab07_q01 {
-
-    
-    //Function for get inputs from user
-    def getUserInputs(count: Int): List[Int] = {
-
-        //Initialize a empty ListBuffer to collect user inputs
-        val numbers = ListBuffer[Int]()
-
-        //loop for get inputs
-        for( i <- 0 until count) {
-            println(s"Enter number ${i+1}: ")
-            val input = readInt()
-            numbers += input //Append the input to the ListBuffer
-        }
-
-        // Convert the ListBuffer to an immutable List and return
-        numbers.toList
-        
-    }
-
-    //Function for filter even numbers entered by user
+object q1 {
+  
     def filterEvenNumbers(numbers: List[Int]): List[Int] = {
-        numbers.filter(n => n%2 ==0)
+        numbers.filter(num => num % 2 == 0)
     }
 
-    //Function for calculate square of numbers entered by user
     def calculateSquare(numbers: List[Int]): List[Int] = {
-        numbers.map((n:Int) => n*n)
+        numbers.map(num => num * num)
     }
 
-    //Function for filter prime numbers entered by user
+    def isPrime(num: Int): Boolean = {
+        if (num <= 1) false
+        else if (num == 2) true
+        else !(2 until num).exists(i => num % i == 0)
+    }
+
     def filterPrime(numbers: List[Int]): List[Int] = {
-        val isPrime: Int => Boolean = n =>{ //isPrime takes Int as input and return a Boolean
-            if(n<=1) false //Numbers less than or equal to 1 are not prime
-            else !(2 to Math.sqrt(n).toInt).exists(i=>n%i==0)
-        }
-
-        numbers.filter(isPrime)
+        numbers.filter(num => isPrime(num))
     }
-
-    def main(args: Array[String]): Unit =  {
-        println("How many numbers do you want to enter?")
-        val count = readInt()
-
-        //Get user inputs as a list
-        val userInputs = getUserInputs(count)
-
-        //Filter the even numbers from the list
-        val selected_even_numbers = filterEvenNumbers(userInputs)
-
-        //Print even numbers
-        println("Even numbers you have entered: ")
-        println(selected_even_numbers)
-
-        //Print square of numbers
-        println("Square of numbers you have entered: ")
-        val square_numbers = calculateSquare(userInputs)
-        println(square_numbers)
-
-        //Print prime numbers
-        println("Prime numbers you have entered: ")
-        val prime_numbers = filterPrime(userInputs)
-        println(prime_numbers)
-
+    
+    def main(args: Array[String]): Unit = {
+        val input1 = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        val input2 = List(1, 2, 3, 4, 5)
+        val output1 = filterEvenNumbers(input1)
+        val output2 = calculateSquare(input2)
+        val output3 = filterPrime(input1)
+        println(output1) 
+        println(output2) 
+        println(output3) 
     }
-
 }
