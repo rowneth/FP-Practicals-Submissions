519200e4ac100901e9dfd078048167cddd549fa4
Don Roneth <113205821+rowneth@users.noreply.github.com>
2024-08-20 09:50:06 +0530

    Create q1.scala

---
+++ Practical09/q1.scala
@@ -0,0 +1,18 @@
+object Practical09_1 {
+  def main(args: Array[String]): Unit = {
+    println("Enter the deposit money:")
+    val userInput = scala.io.StdIn.readLine()
+    val interestAmount = interest(userInput.toDouble)
+    println(s"The actual amount of interest is: $interestAmount")
+  }
+
+  def interest(deposit: Double): Double = {
+    val interestRate: Double => Double = deposit match {
+      case d if d <= 20000 => _ => 0.02
+      case d if d <= 200000 => _ => 0.04
+      case d if d <= 2000000 => _ => 0.035
+      case _ => _ => 0.065
+    }
+    deposit * interestRate(deposit)
+  }
+}
